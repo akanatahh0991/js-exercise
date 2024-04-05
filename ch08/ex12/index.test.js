4fb9ef880b66ba48b6e8d9e2fadd7da8ba336e32
Hiroatsu Hatanaka <hiroatsu.hatanaka@jp.ricoh.com>
2024-04-05 17:46:51 +0900

    テスト入れ替え

--- ch08/ex12/index.test.js
+++ ch08/ex12/index.test.js
@@ -1,52 +1,32 @@
-import {f} from "./index.js";
+import { f } from "./index.js";
 
-test.each([
-  {
-    type: "normal", 
-    stringAction: "$1 + $2",
-    args: [1, 2],
-    result: 3
-  },
-  {
-    type: "normal", 
-    stringAction: "$1 + $2 + $1",
-    args: [1, 2],
-    result: 4
-  },
-  {
-    type: "normal", 
-    stringAction: "$1 + $3 + $5",
-    args: [1, undefined , 2, undefined ,3],
-    result: 6
-  },
-  {
-    type: "normal", 
-    stringAction: "$1 + $2 + $3 + $4 + $5 + $6 + $7 + $8 + $9 + $10",
-    args: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    result: 55
-  },
-  {
-    type: "normal", 
-    stringAction: "100",
-    args: [],
-    result: 100
-  },
-  {
-    type: "abnormal", 
-    stringAction: "$0 + $1",
-    args: [1, 2],
-    result: undefined
-  },
-  {
-    type: "abnormal", 
-    stringAction: "$1 + $11",
-    args: [1, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 11],
-    result: undefined
-  }
-])("$type test f($stringAction)(...$args) => $result", ({type, stringAction, args, result}) => {
-  if (type === 'normal') {
-    expect(f(stringAction)(...args)).toBe(result);
-  } else {
-    expect(() => f(stringAction)(...args)).toThrow();
-  }
-})
+describe("f", () => {
+  it("returns function with no args", () => {
+    expect(f("42")()).toBe(42);
+  });
+
+  it("returns function with one arg", () => {
+    expect(f("$1 + 1")(1)).toBe(2);
+  });
+
+  it("returns function with multi args", () => {
+    expect(
+      f("$1 + $2 + $3 + $4 + $5 + $6 + $7 + $8 + $9 + $10")(
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz"
+      )
+    ).toBe("12Fizz4BuzzFizz78FizzBuzz");
+  });
+
+  it("returns function with multi line", () => {
+    expect(f("{ const result = $1 + $2;\n return result; }")(1, 2)).toBe(3);
+  });
+});
